008c5c9b64d96db238df6dca5bcfce175a9e7808
ryan <rtreacy87@gmail.com>
2024-07-27 23:00:29 -0600

    put the plugins into their own folders

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,7 @@
+return { -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require('which-key').setup()
+  end,
+}
