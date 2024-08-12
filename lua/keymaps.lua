9bc6425f6a5a36ff02713521d8773bca93ea1d79
Ryan Treacy <rtreacy87@gmail.com>
2024-08-11 19:00:59 -0600

    added packaged to interact with tmux

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -28,6 +28,8 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
+--
+--
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- lua/plugins/tmux-vim.lua
+++ lua/plugins/tmux-vim.lua
@@ -1,17 +1,11 @@
 return {
   'christoomey/vim-tmux-navigator',
-  cmd = {
-    'TmuxNavigateLeft',
-    'TmuxNavigateDown',
-    'TmuxNavigateUp',
-    'TmuxNavigateRight',
-    'TmuxNavigatePrevious',
-  },
-  keys = {
-    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-  },
+  lazy = false,
+  config = function()
+    vim.keymap.set('n', '<c-h>', ':<C-U>TmuxNavigateLeft<cr>', { silent = true })
+    vim.keymap.set('n', '<c-j>', ':<C-U>TmuxNavigateDown<cr>', { silent = true })
+    vim.keymap.set('n', '<c-k>', ':<C-U>TmuxNavigateUp<cr>', { silent = true })
+    vim.keymap.set('n', '<c-l>', ':<C-U>TmuxNavigateRight<cr>', { silent = true })
+    vim.keymap.set('n', '<c-\\>', ':<C-U>TmuxNavigatePrevious<cr>', { silent = true })
+  end,
 }
